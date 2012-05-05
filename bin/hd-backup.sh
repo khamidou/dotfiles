1c02ed3d7de03717777901f9c3015f2ddb48df38
karim hamidou <khamidou@gmail.com>
2012-05-05 11:59:42 +0200

    Added backup script. Added .bin to the tcsh path.

---
+++ bin/hd-backup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# backup my home directory using rsync.
+
+rsync --verbose  --progress --stats --compress --rsh=/usr/bin/ssh \
+      --recursive --times --perms --links --delete \
+      --exclude "*~" \
+      --exclude ".*" \
+      ~ "/media/My Passport"
