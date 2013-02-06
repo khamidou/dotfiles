8b335c8c74bc3213d9c5e18a29c594f6dd903c47
karim hamidou <khamidou@gmail.com>
2013-02-06 09:20:54 +0100

    replace rakefile by handmade python setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# setup the dotfiles
+
+import sys
+import os
+
+excluded_files = ["Rakefile", "README.rdoc", "LICENSE", "fish", "setup.py"]
+destdir = os.getenv('HOME')
+
+if len(sys.argv) == 2:
+    destdir = sys.argv[1]
+
+entries = [file for file in os.listdir(".") if file not in excluded_files]
+
+for dentry in entries:
+    linkpath = os.path.join(destdir, "." + dentry) 
+    if not os.path.exists(linkpath):
+        str ="ln -s %s %s" % (os.path.realpath(dentry), linkpath) 
+        print str
+        os.system(str)
+
