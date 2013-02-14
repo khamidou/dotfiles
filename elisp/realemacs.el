b7e8ef1fd7b7bef66b7895ae97f04e828d944c53
karim hamidou <khamidou@gmail.com>
2013-02-14 09:48:07 +0100

    latest changes

--- elisp/realemacs.el
+++ elisp/realemacs.el
@@ -9,6 +9,17 @@
 
 (setq backup-directory-alist `(("." . "~/.emacs_saves"))) ;; put backups in specific dir
 
+(setq tab-width 4)
+(setq tab-stop-list (number-sequence 4 120 4))
+(setq indent-tabs-mode nil)
+
+;html-mode changes
+(add-hook 'sgml-mode-hook
+	  (lambda ()
+	    ;; Default indentation to 2, but let SGML mode guess, too.
+	    (set (make-local-variable 'sgml-basic-offset) 2)
+	    (sgml-guess-indent)))
+
 (require 'color-theme)
 (color-theme-initialize)
 (color-theme-arjen)
@@ -39,6 +50,10 @@
 
 (require 'smart-tab)
 (global-smart-tab-mode 1)
+
+;; persist minibuffer history
+(savehist-mode 1)
+(setq savehist-additional-variables '(kill-ring search-ring))
 
 
 
