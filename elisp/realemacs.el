068fc5499635be9ce3ff010e17efc355aa5f664e
karim hamidou <khamidou@gmail.com>
2013-04-07 11:57:01 +0200

    spring cleaning

--- elisp/realemacs.el
+++ elisp/realemacs.el
@@ -3,21 +3,23 @@
 ;; add all the subdirs of ~/.elisp to load path
 (let ((default-directory "~/.elisp/"))
   (normal-top-level-add-subdirs-to-load-path))
-  
+
 (global-font-lock-mode 1) ; syntax highlighting
 (ido-mode)
 
 (setq backup-directory-alist `(("." . "~/.emacs_saves"))) ;; put backups in specific dir
 
+(setq-default indent-tabs-mode nil)
 (setq tab-width 4)
 (setq tab-stop-list (number-sequence 4 120 4))
-(setq indent-tabs-mode nil)
 
 ;html-mode changes
 (add-hook 'sgml-mode-hook
 	  (lambda ()
-	    ;; Default indentation to 2, but let SGML mode guess, too.
-	    (set (make-local-variable 'sgml-basic-offset) 2)
+	    ;; Default indentation to 4, but let SGML mode guess, too.
+	    (set (make-local-variable 'sgml-basic-offset) 4)
+	    (setq indent-tabs-mode nil)
+;;	    (setq indent-line-function 'indent-relative)
 	    (sgml-guess-indent)))
 
 (require 'color-theme)
@@ -54,8 +56,6 @@
 ;; persist minibuffer history
 (savehist-mode 1)
 (setq savehist-additional-variables '(kill-ring search-ring))
-
-
 
 (add-hook 'emacs-startup-hook (lambda ()
                                 (message "Time needed to load: %s seconds."
