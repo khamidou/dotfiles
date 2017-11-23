bef2b0d9407887d289df255440c3520d7b52de71
Karim Hamidou <khamidou@gmail.com>
2017-11-23 15:26:38 -0800

    Fix .emacs

--- elisp/realemacs.el
+++ elisp/realemacs.el
@@ -26,24 +26,6 @@
 ;; (color-theme-initialize)
 ;; (color-theme-arjen)
 
-;; evil config
-(require 'evil)
-
-(setq evil-default-state 'normal) ; as close as vim as possible
-;;; esc quits everything, everywhere
-
-(define-key evil-normal-state-map [escape] 'keyboard-quit)
-(define-key evil-visual-state-map [escape] 'keyboard-quit)
-(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
-
-(define-key evil-normal-state-map "gb" 'ido-switch-buffer)
-(define-key evil-normal-state-map "gf" 'ido-find-file)
-(evil-mode 1)
-
 ;; various requires
 (require 'cparen)
 (cparen-activate)
@@ -56,7 +38,3 @@
 ;; persist minibuffer history
 (savehist-mode 1)
 (setq savehist-additional-variables '(kill-ring search-ring))
-
-(add-hook 'emacs-startup-hook (lambda ()
-                                (message "Time needed to load: %s seconds."
-					 (emacs-uptime "%s"))) 'append)
